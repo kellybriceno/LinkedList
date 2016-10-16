271511cc36c8f4494933a8b6d9d2649471861683
kelly briceno <kellygavin96@gmail.com>
2016-10-16 15:10:02 -0400

    created linked list class to store methods and main method

--- LinkedList.java
+++ LinkedList.java
@@ -1,5 +1,16 @@
 //Kelly Briceno
 //ICSI 213 linkedlist assignment
 
-public class LinkedList {
+public class LinkedList { 
+static int noOfLinkedList = 0;
+  
+  int data;
+  
+  DNode previousNode;
+  DNode nextNode;
+  
+  DNode (int data) {
+    
+     this.data = data;
+     noOfLinkedList++;
 }
