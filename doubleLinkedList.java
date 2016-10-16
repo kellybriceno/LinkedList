d55961555e94d7a9f924b315061dde86eaa1ce4e
kelly briceno <kellygavin96@gmail.com>
2016-10-16 15:10:23 -0400

    inserted methods to later put in the data for the methods

---
+++ doubleLinkedList.java
@@ -0,0 +1,27 @@
+public class doubleLinkedList {
+  static linkNode root; 
+  static linkNode temp;
+  static linkNode current;
+  
+  
+  public void addNodes(int data) {
+  }
+  
+  
+   public void insertNode(int data, int after) {
+   }
+   
+   
+    public void deleteNode(int nodeString) {
+    }
+    
+    
+    public void print() {
+    }
+    
+    public void printBackwards() {
+      
+      }
+    public static void main(String [] args) {
+    }
+    
